777c46bfa7fb414bbb015b85c3013af2d4476140
Noah Marcus <noah@innovation.nj.gov>
2025-12-01 12:10:28 -0500

    Update oh-my-zsh installation to be unattended
    Add unattended flag to oh-my-zsh installation. Current behavior causes oh-my-zsh to launch and stop the script from running. We may need to advise team members to restart/refresh their terminals after running the script.
    
    https://github.com/ohmyzsh/ohmyzsh?tab=readme-ov-file#unattended-install

--- scripts/unix.sh
+++ scripts/unix.sh
@@ -7,7 +7,7 @@ brew install wget
 
 if [ -z "$ZSH" ]; then
   echo "Installing oh-my-zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
   echo "oh-my-zsh is already installed!"
 fi
