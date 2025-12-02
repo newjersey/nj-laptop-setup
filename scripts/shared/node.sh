b07d46fe36befa23477ff91357a2b4dbf2c429fe
Anne LoVerso <anne.loverso@gmail.com>
2025-12-02 12:08:03 -0500

    check for nvm existing install

--- scripts/shared/node.sh
+++ scripts/shared/node.sh
@@ -1,11 +1,19 @@
 echo
 echo "Installing nvm and Node"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm install node
 
-current=$(nvm current)
-newly_installed+=("nvm: Node $current")
+if [ -d "${NVM_DIR}/.git" ]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    nvm install node
+    current=$(nvm current)
+    already_installed+=("nvm: Node $current")
+else        
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+    
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+    nvm install node
+    current=$(nvm current)
+    newly_installed+=("nvm: Node $current")
+fi
