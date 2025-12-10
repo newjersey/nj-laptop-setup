82492a4deac4400f8f1019ffceb7a0f2cb1111b0
Case Walker <25988138+casewalker@users.noreply.github.com>
2025-12-09 22:00:05 -0500

    Simplify AWS CLI installation

--- scripts/shared/awscli.sh
+++ scripts/shared/awscli.sh
@@ -1,8 +1,11 @@
-if is_brew_installed awscli; then
-    already_installed+=("awscli")
-else
-    check_and_install_brew awscli
-    echo "Setting up defaults for AWS CLI..."
+check_and_install_brew awscli
+
+echo "Setting up defaults for AWS CLI..."
+default_region=$(aws configure get default.region)
+if [ -z $default_region ]; then
     aws configure set default.region us-east-1
+fi
+default_output=$(aws configure get default.output)
+if [ -z $default_output ]; then
     aws configure set default.output json
 fi
