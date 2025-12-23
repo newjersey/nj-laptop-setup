902639c1abe74ef7085de8093d9a8eefa83d0fa0
Case Walker <25988138+casewalker@users.noreply.github.com>
2025-12-23 09:16:37 -0500

    Disable AWS CLI default configuration setup

--- scripts/shared/awscli.sh
+++ scripts/shared/awscli.sh
@@ -1,11 +1,12 @@
 check_and_install_brew awscli
 
-echo "Setting up defaults for AWS CLI..."
-default_region=$(aws configure get default.region)
-if [ -z $default_region ]; then
-    aws configure set default.region us-east-1
-fi
-default_output=$(aws configure get default.output)
-if [ -z $default_output ]; then
-    aws configure set default.output json
-fi
+## SKIP - Case encountered issues with this :( sadness
+# echo "Setting up defaults for AWS CLI..."
+# default_region=$(aws configure get default.region)
+# if [ -z $default_region ]; then
+#     aws configure set default.region us-east-1
+# fi
+# default_output=$(aws configure get default.output)
+# if [ -z $default_output ]; then
+#     aws configure set default.output json
+# fi
